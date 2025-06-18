e586ac146220c605bdd019ebfb07601f627b57bd
unknown <gabrielmarvelous1@gmail.com>
2025-06-18 10:21:55 +0100

    updated the code

--- app/controllers/searchController.php
+++ app/controllers/searchController.php
@@ -3,57 +3,62 @@
     require_once __DIR__ . '/../models/products.php';
     require_once __DIR__ . '/../models/users.php';
     if(isset($_GET['search'])){
-        require_once __DIR__ . '/../utils/features.php';
-        $searchField = htmlspecialchars($_GET['search']);
-        $params = ['search' => $searchField];
-        $search = http_build_query($params);
-        $result = Product::query()
-            -> search($searchField, ['product_name', 'username'])
-            ->getWithPagination();
-        $data = $result['data'];
-        $matchedOn = 'unknown';
-        if(count($data) > 0){
-            foreach($data as $row){
-                if (stripos($row['product_name'], $searchField) !== false) {
-                    $matchedOn = 'product';
-                    if (strpos($_SERVER['REQUEST_URI'], '/products') === false) {
-                        header('Location: '. BASE_PATH .'products?'.$search);
-                        exit();
-                    }
-
-                    
-                } elseif (stripos($row['username'], $searchField) !== false) {
-                    $matchedOn = 'vendor';
-                    if (strpos($_SERVER['REQUEST_URI'], '/vendors') === false) {
-                        header('Location: '. BASE_PATH .'vendors?'.$search);
-                        exit();
-                    }
-                }
-            }
-
-        }else{
-            $result = Category::query()
-                -> search($searchField, ['name', 'slug'])
+        try{
+            require_once __DIR__ . '/../utils/features.php';
+            $searchField = htmlspecialchars($_GET['search']);
+            $params = ['search' => $searchField];
+            $search = http_build_query($params);
+            $result = Product::query()
+                -> search($searchField, ['product_name', 'username'])
                 ->getWithPagination();
             $data = $result['data'];
+            $matchedOn = 'unknown';
             if(count($data) > 0){
                 foreach($data as $row){
-                    $result = Product::query()
-                        -> search($searchField, ['product_name', 'username'])
-                        ->getWithPagination();
-                    $data = $result['data'];
+                    if (stripos($row['product_name'], $searchField) !== false) {
+                        $matchedOn = 'product';
+                        if (strpos($_SERVER['REQUEST_URI'], '/products') === false) {
+                            header('Location: '. BASE_PATH .'products?'.$search);
+                            exit();
+                        }
+
+                        
+                    } elseif (stripos($row['username'], $searchField) !== false) {
+                        $matchedOn = 'vendor';
+                        if (strpos($_SERVER['REQUEST_URI'], '/vendors') === false) {
+                            header('Location: '. BASE_PATH .'vendors?'.$search);
+                            exit();
+                        }
+                    }
                 }
-                $matchedOn = 'category';
-                if (strpos($_SERVER['REQUEST_URI'], '/products/category') === false) {
-                    header('Location: '. BASE_PATH . 'products/category?'.$search);
-                    exit();
-                }
+
             }else{
-                if (strpos($_SERVER['REQUEST_URI'], '/products') === false) {
-                    header('Location: '. BASE_PATH . 'products?search='.$search);
+                $result = Category::query()
+                    -> search($searchField, ['name', 'slug'])
+                    ->getWithPagination();
+                $data = $result['data'];
+                if(count($data) > 0){
+                    foreach($data as $row){
+                        $result = Product::query()
+                            -> search($searchField, ['product_name', 'username'])
+                            ->getWithPagination();
+                        $data = $result['data'];
+                    }
+                    $matchedOn = 'category';
+                    if (strpos($_SERVER['REQUEST_URI'], '/products/category') === false) {
+                        header('Location: '. BASE_PATH . 'products/category?'.$search);
+                        exit();
+                    }
+                }else{
+                    if (strpos($_SERVER['REQUEST_URI'], '/products') === false) {
+                        header('Location: '. BASE_PATH . 'products?search='.$search);
+                    }
                 }
             }
+            $_SESSION['data'] = $data;
+        }catch(Exception $e){
+            echo('error: ');
+            echo $e -> getMessage();
         }
-        $_SESSION['data'] = $data;
     };
 ?>
